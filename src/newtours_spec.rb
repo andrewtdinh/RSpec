7ed6947d3ddf8980eddde8c688bbad6e420b25e5
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-17 19:18:46 -0700

    Add a test case to check user cannot sign in with a wrong password from the Sign-On page.

--- src/newtours_spec.rb
+++ src/newtours_spec.rb
@@ -90,6 +90,24 @@ describe 'Newtours site' do
     expect(pageTitle).to be == 'Sign-on: Mercury Tours'
   end
 
+  it 'should not be able to sign into the site from the Sign-On page with wrong pass word' do
+    # click on Sign-On link
+    @driver.find_element(:link, 'SIGN-ON').click
+
+    @wait.until { @driver.find_element(:name, 'login')}
+
+    # Type into the Username and Password fields:
+    @driver.find_element(:name, 'userName').send_key 'AndrewD'
+    @driver.find_element(:name, 'password').send_key 'wrong_pass'
+    @driver.find_element(:name, 'login').click
+
+    #Get and verify the page title after a 5-second wait
+    sleep(5)
+    pageTitle = @driver.title
+    expect(pageTitle).to be == 'Sign-on: Mercury Tours'
+  end
+
+
   after(:each) do
     # finally, close the browser
     @driver.quit
